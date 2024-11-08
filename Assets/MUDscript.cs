e6770948935921e44d82ac595e59b91efec754fe
Robin B. <70322796+RoboOlaf@users.noreply.github.com>
2024-11-08 15:57:45 +0100

    Mudding

--- Assets/MUDscript.cs
+++ Assets/MUDscript.cs
@@ -4,27 +4,55 @@ using FMODUnity;
 public class MUDscript : MonoBehaviour
 {
     public float mud = 0;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
+    public float start = 1;
+    
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(("space")) && start == 1)
+        {   
+            _animator.Play("cleanDirt");
+            Debug.Log("MUDING");
+            start = 0;
+        }
+        
+        else if (Input.GetKeyDown("space") && (mud == 1))
         {
-            mud += 1f;
+            _animator.Play("dirtyDirt");
             Debug.Log("MUDscript" + mud);
-            RuntimeManager.PlayOneShot("event:/Digg Mud Right");
-            RuntimeManager.PlayOneShot("event:/Digg Mud Left");
+        }
+        
+        if (Input.GetKeyDown("m"))
+        {
+            Debug.Log("MUDscript" + mud);
+            Splash();
         }
     }
 
     public void MUD()
     {
-        
+        mud = 0;
+        RuntimeManager.PlayOneShot("event:/Digg Mud Right");
+        RuntimeManager.PlayOneShot("event:/Digg Mud Left");
+    }
+
+    public void Splash()
+    {
+        RuntimeManager.PlayOneShot("event:/Dropp Mud Right");
+        RuntimeManager.PlayOneShot("event:/Dropp Mud Left");
+    }
+
+    public void Mudreaddy()
+    {
+        mud = 1;
+        _animator.Play("dirtyIdle");
     }
 }
